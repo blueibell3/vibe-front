b6cccf5dc86853ec4daa8299cdb8ff85efefaddf
giorgitsintsadzee <giorgi.tsintsadze26@gmail.com>
2024-07-10 16:55:07 +0400

    feat: heart icon

---
+++ app/Components/LikeButton/LikeButton.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image"
+
+export default () => {
+
+    return(
+        <div>
+            <Image src={'/emptyHeart.svg'} alt="emepty like" width={24} height={24}/>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
 import styles from './page.module.css'
+import LikeButton from "./Components/LikeButton/LikeButton";
 
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <LikeButton/>
+    </div>
   );
 }
 
