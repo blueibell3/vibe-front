ad46b997d942842ff68a96f69416400b58319bc2
giorgitsintsadzee <giorgi.tsintsadze26@gmail.com>
2024-07-11 19:22:06 +0400

    create reusable text button

---
+++ app/Components/TextButton/TextButton.tsx
@@ -0,0 +1,14 @@
+import styles from './TextButton.module.scss'
+type Props = {
+    title: string;
+    mode?: 'fill' | 'text';
+    width?: string;
+}
+export default (props: Props) => {
+    const classes = []
+    if (props.mode == 'text') classes.push(styles.text)
+    else classes.push(styles.fill)
+    return (
+        <button style={{ width: `${props.width}` }} className={classes.join('').trim()}>{props.title}</button>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import styles from './page.module.css'
 import LikeButton from "./Components/LikeButton/LikeButton";
+import TextButton from "./Components/TextButton/TextButton";
 
 export default function Home() {
   return (
     <div>
- 
     </div>
   );
 }
