336040f03748f16704e2c8a44131f1a2e82ea9a5
lizi qerdikashvili <lizi.qerdikashvili@icloud.com>
2024-07-11 19:48:05 +0400

    create reusable button component

--- app/Components/Button/Button.tsx
+++ app/Components/Button/Button.tsx
@@ -5,19 +5,18 @@ type Props = {
     title: string;
     type: "primary" | "secondary";
     width?: string;
-    color?: string;
-    className: string;
 }
 
 export default (props: Props) => {
 
-    const classes = [styles.button, props.className]
+    const classes = [styles.button]
+    
     if (props.type == 'secondary') classes.push(styles.secondary)
     else classes.push(styles.primary)
 
     return (
         <button
-            className={classes.join(' ').trim()}>
+            style={{width: `${props.width}` }} className={classes.join(' ').trim()}>
             {props.title}
         </button>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,7 @@ import Button from "./Components/Button/Button";
 export default function home() {
   return (
    <div className={styles.container}>
-    <Button type="secondary" title="Cancel" className={styles.secondary}></Button>
-    <Button type="primary" title="Done" className={styles.primary}></Button>
+   
    </div>
   );
 }
