505140e4dacc84d760ce365d6c2ab85cb78ebc79
lizi qerdikashvili <lizi.qerdikashvili@icloud.com>
2024-07-11 15:02:30 +0400

    fix: added

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from './page.module.css'
-import Button from "./Components/Button";
+import Button from "./Components/Button/Button";
 
 export default function home() {
   return (
