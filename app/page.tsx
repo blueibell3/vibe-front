ecb3e7c0d79ec50127978aaa526c0873b6ed6c48
lizi qerdikashvili <lizi.qerdikashvili@icloud.com>
2024-07-12 18:22:31 +0400

    created music card component

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,21 @@ import Image from "next/image";
 import styles from './page.module.css'
 import LikeButton from "./Components/LikeButton/LikeButton";
 import TextButton from "./Components/TextButton/TextButton";
+import MusicCard from "./Components/MusicCard/MusicCard";
+
 
 export default function home() {
   return (
-    <div>
-    </div>
+    <>
+      <div>
+        <MusicCard
+          imageUrl="https://upload.wikimedia.org/wikipedia/commons/1/1f/Compact_disc_album.jpg"
+          songName="Yellow"
+          artistName="The Beatles"
+          audioPlay='/Group.svg'
+        />
+      </div>
+      <LikeButton/>
+    </>
   );
 }
-
