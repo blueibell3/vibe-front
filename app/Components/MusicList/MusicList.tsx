1e5cbbde6df3100f59c064122c5ff9a9992e1d76
lizi qerdikashvili <lizi.qerdikashvili@icloud.com>
2024-07-14 01:58:56 +0400

    Create Music List Component

---
+++ app/Components/MusicList/MusicList.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import styles from "../MusicList/MusicList.module.scss";
+
+
+
+const musicData = [
+    {
+        imageUrl: 'https://daprintfactory.com/cdn/shop/files/music-cds-music-covers-print-1.png?v=1690954711',
+        songName: 'Unwritten',
+        artistName: 'Natasha Bedingfield',
+        time: '4:17',
+    },
+    {
+      
+        imageUrl: 'https://daprintfactory.com/cdn/shop/files/music-cds-music-covers-print-1.png?v=1690954711',
+        songName: 'Sledgehammer',
+        artistName: 'Fifth Harmony',
+        time: '3:49',
+    },
+    {
+        imageUrl: 'https://daprintfactory.com/cdn/shop/files/music-cds-music-covers-print-1.png?v=1690954711',
+        songName: 'You will never know',
+        artistName: 'Imany',
+        time: '3:00',
+    },
+    {
+        imageUrl: 'https://daprintfactory.com/cdn/shop/files/music-cds-music-covers-print-1.png?v=1690954711',
+        songName: 'Song Sign of the times',
+        artistName: 'Harry Styles',
+        time: '5:49',
+    },
+    {
+        imageUrl: 'https://daprintfactory.com/cdn/shop/files/music-cds-music-covers-print-1.png?v=1690954711',
+        songName: 'Song Mockingbird',
+        artistName: 'Eminem',
+        time: '4:11',
+    },
+    {
+        imageUrl: 'https://daprintfactory.com/cdn/shop/files/music-cds-music-covers-print-1.png?v=1690954711',
+        songName: 'Here comes the sun Two',
+        artistName: 'The Beatles',
+        time: '3:06',
+    },
+    {
+        imageUrl: 'https://daprintfactory.com/cdn/shop/files/music-cds-music-covers-print-1.png?v=1690954711',
+        songName: 'As it was',
+        artistName: 'Harry styles',
+        time: '2:47',
+    },
+    {
+        imageUrl: 'https://daprintfactory.com/cdn/shop/files/music-cds-music-covers-print-1.png?v=1690954711',
+        songName: 'Sharks',
+        artistName: 'Imagine Dragons',
+        time: '3:00',
+    },
+];
+
+
+type Props = {
+    imageUrl: string;
+    songName: string;
+    artistName: string;
+    onPlay: () => void;
+    time: string;
+}
+
+export default (props: Props) => {
+
+
+    return (
+        <div className={styles.MusicListCategory}>
+            <div className={styles.nextPlay}>Next Play</div>
+            {musicData.map(category => (
+                <div className={styles.MusicListId}>
+                    <img className={styles.MusicListimageUrl} src={category.imageUrl} alt="imageUrl" />
+                    <div className={styles.MusicListText}>
+                        <div className={styles.MusicListNames}>
+                            <div className={styles.songName}>{category.songName}</div>
+                            <div className={styles.artistName}>{category.artistName}</div>
+                        </div>
+                        <span className={styles.MusicListTime}>{category.time}</span>
+                    </div>
+                </div>
+            ))}
+
+        </div>
+    )
+};
--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,22 @@ import Image from "next/image";
 import styles from './page.module.css'
 import LikeButton from "./Components/LikeButton/LikeButton";
 import TextButton from "./Components/TextButton/TextButton";
+import MusicList from "./Components/MusicList/MusicList";
 
 export default function home() {
+
+  const handlePlay = () => {
+    console.log('Playing song');
+  };
+
   return (
-    <div>
-    </div>
+    <MusicList 
+    imageUrl={""} 
+    songName={""}
+     artistName={""}
+     time={""} 
+     onPlay={handlePlay}
+     />
   );
 }
 
