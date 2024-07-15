3aaeb0de872e1dae7d90aa761523a1901433be36
giorgitsintsadzee <giorgi.tsintsadze26@gmail.com>
2024-07-15 14:26:51 +0400

    create user avatar

--- app/Components/Header/Header.tsx
+++ app/Components/Header/Header.tsx
@@ -3,17 +3,20 @@ import styles from './Header.module.scss'
 import UserAvatar from './UserAvatar/UserAvatar'
 
 export default () => {
+    let longout = '/longout icoon.svg'
     return (
         <div className={styles.container} >
             <div className={styles.searchLogo}>
-                <Image src='/logo.svg' alt='logo' width={91} height={39} />
-                <div className={styles.searchContainer}>
-                    <input className={styles.search} type='search' placeholder='Search' />
-                    <Image src='/search icon.svg' width={24} height={24} alt='search icon' className={styles.icon} />
+                <div className={styles.zzzz}>
+                    <Image src='/logo.svg' alt='logo' width={91} height={39} />
+                    <div className={styles.searchContainer}>
+                        <input className={styles.search} type='' placeholder='Search' />
+                        <Image src='/search icon.svg' width={24} height={24} alt='search icon' className={styles.icon} />
+                    </div>
                 </div>
             </div>
             <div>
-                <UserAvatar />
+                <UserAvatar text='Log Out' url={longout} />
             </div>
         </div>
     )
--- app/Components/Header/UserAvatar/UserAvatar.tsx
+++ app/Components/Header/UserAvatar/UserAvatar.tsx
@@ -3,20 +3,30 @@ import { useState } from 'react'
 import styles from './UserAvatar.module.scss'
 import Image from 'next/image'
 
-export default () => {
+type Props = {
+    url?: string;
+    text: string
+}
+
+export default (props: Props) => {
     const [userIn, setUserIn] = useState(false)
     const onChange = () => {
         setUserIn(!userIn)
     }
     return (
-        <div className={ styles.container}>
-            <Image src='/user avatar.svg' onClick={onChange} width={32} height={32} alt='user avatar' />
-            {
-                userIn &&
+        <>
+        <div className={styles.container}>
+            <Image src='/user avatar.svg' onClick={onChange} width={32} height={32} alt='user avatar' className={styles.userAvatar} />
+            {userIn &&
+
                 <div className={styles.userWindow}>
-                    <span>sdsadd</span>
-                </div>
-            }
+                    <img src={props.url} alt='longout button' width={24} height={24} />
+                    {props.text}
+                </div>}
         </div>
+        <div className={styles.burgerMenu}>
+                <img src="/menu-burger.svg" alt="burger menu" width={44.02} height={44.02} />
+            </div>
+            </>
     )
 }
