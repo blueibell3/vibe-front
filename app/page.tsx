7133b547bd55bc4e5f9732c9d4f4ebc21d8c35cd
lizi qerdikashvili <lizi.qerdikashvili@icloud.com>
2024-07-15 18:50:19 +0400

    fix page

--- app/page.tsx
+++ app/page.tsx
@@ -13,28 +13,7 @@ export default function home() {
 
   return (
    <>
-    <div className={styles.chartCardContainer}>
-    <ChartCard
-      title='top hits 2024'
-      id={1}
-      imageUrl='https://media.architecturaldigest.com/photos/5890e88033bd1de9129eab0a/1:1/w_870,h_870,c_limit/Artist-Designed%20Album%20Covers%202.jpg'
-    />
-     <ChartCard
-      title='top hits 2024'
-      id={2}
-      imageUrl='https://media.architecturaldigest.com/photos/5890e88033bd1de9129eab0a/1:1/w_870,h_870,c_limit/Artist-Designed%20Album%20Covers%202.jpg'
-    />
-     <ChartCard
-      title='top hits 2024'
-      id={3}
-      imageUrl='https://media.architecturaldigest.com/photos/5890e88033bd1de9129eab0a/1:1/w_870,h_870,c_limit/Artist-Designed%20Album%20Covers%202.jpg'
-    />
-     <ChartCard
-      title='top hits 2024'
-      id={4}
-      imageUrl='https://media.architecturaldigest.com/photos/5890e88033bd1de9129eab0a/1:1/w_870,h_870,c_limit/Artist-Designed%20Album%20Covers%202.jpg'
-    />
-    </div>
+   
    </>
 
   );
