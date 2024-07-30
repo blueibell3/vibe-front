d61a7fdb8de2b5024f1b6d3ec448c4ad3406c4b1
lizi qerdikashvili <lizi.qerdikashvili@icloud.com>
2024-07-30 18:59:44 +0400

    fix import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import styles from "./page.module.css";
+import styles from "./ (authorised)/page.module.css";
 import NavBarMenu from "./Components/NavBarMenu/NavBarMenu";
 import Header from "./Components/Header/Header";
 import BurgerMenu from "./Components/BurgerMenu/BurgerMenu";
