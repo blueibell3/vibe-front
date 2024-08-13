cf082b7a98f5de35079e83d149ce89f0ac1a1318
giorgitsintsadzee <giorgi.tsintsadze26@gmail.com>
2024-08-13 16:22:31 +0400

    refactor searchBar component

--- app/Components/Header/SearchBar/ListOptions/ListOptions.tsx
+++ app/Components/Header/SearchBar/ListOptions/ListOptions.tsx
@@ -1,35 +1,44 @@
+import Link from 'next/link';
 import styles from './ListOptions.module.scss';
 
 type Props = {
     id: number;
     text: string;
     img: string;
-    type: string;
-}
+    type: 'singer' | 'album';
+    link: string;
+};
 
 interface ListOptionsProps {
     options: Props[];
-    onOptionClick: (text: string) => void;
+    onOptionClick?: (text: string) => void
 }
+
 const ListOptions = ({ options, onOptionClick }: ListOptionsProps) => {
     return (
         <ul className={styles.listOptions}>
             {options.map(option => (
-                <li
-                    key={option.id}
-                    className={styles.option}
-                    onClick={() => onOptionClick(option.text)}
-                >
-                    <img
-                        src={option.img}
-                        alt={option.text}
-                        className={`${styles.optionImage} ${option.type === 'album' ? styles.album : styles.singer}`}
-                    />
-                    <span>{option.text}</span>
-                </li>
+                <Link href={option.link}>
+                    <li key={option.id} className={styles.option}>
+                        <a
+                            onClick={() => {
+                                if (onOptionClick) {
+                                    onOptionClick(option.text);
+                                }
+                            }}
+                        >
+                            <img
+                                src={option.img}
+                                alt={option.text}
+                                className={`${styles.optionImage} ${styles[option.type]}`}
+                            />
+                            <span>{option.text}</span>
+                        </a>
+                    </li>
+                </Link>
             ))}
         </ul>
     );
-}   
+};
 
 export default ListOptions;
