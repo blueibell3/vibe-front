2f212437b08523b1917a3a0e1f8f90e676117fe8
giorgitsintsadzee <giorgi.tsintsadze26@gmail.com>
2024-08-22 19:30:05 +0400

    done home page

--- app/Components/MusicList/MusicList.tsx
+++ app/Components/MusicList/MusicList.tsx
@@ -30,8 +30,8 @@ const MusicList = (props: Props) => {
         <div className={styles.MusicListCategory} onClick={handleClick}>
             <div className={styles.MusicListId}>
                 <div className={styles.imgCenter}>
-                    <img className={styles.MusicListimageUrl} src={props.imageUrl} alt="imageUrl" />
                     <img src={isPlaying && currentTrackIndex === props.trackIndex ? '/icons/pause.svg' : '/group.svg'} alt="ap" className={styles.audioPlay} />
+                    <img className={styles.MusicListimageUrl} src={props.imageUrl} alt="imageUrl" />
                 </div>
                 <div className={styles.MusicListText}>
                     <div className={styles.MusicListNames}>
