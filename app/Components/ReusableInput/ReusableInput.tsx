3cc8c79c813bd04fe79368782be99a2deb5d82b8
lizi qerdikashvili <lizi.qerdikashvili@icloud.com>
2024-09-04 19:23:20 +0400

    refactor input

--- app/Components/ReusableInput/ReusableInput.tsx
+++ app/Components/ReusableInput/ReusableInput.tsx
@@ -1,55 +1,25 @@
 'use client'
-import { useState } from 'react';
 import styles from './ReusableInput.module.scss';
-import { useForm } from 'react-hook-form';
+import { UseFormRegister, UseFormRegisterReturn, useForm } from 'react-hook-form';
 
 type Props = {
     type: 'email' | 'password';
     placeholder: string;
+    error?: string; 
+    mode?: 'standard' | 'error' | 'success';
+    register: UseFormRegisterReturn;
 };
 
-const ReusableInput = (props: Props) => {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors, isValid},
-    } = useForm();
 
-    const inputData = props.type === 'email'
-        ? {
-            required: true,
-            pattern: {
-                value: /\S+@\S+\.\S+/,
-                message: "Entered value does not match email format",
-            },
-        }
-        : { required: true, minLength: 8 };
-
-
-    let mode = 'standard';
-    if (errors[props.type]) {
-        mode = 'error';
-    } else if (isValid) {
-        mode = 'success';
-    }
-
-    const onSubmit = () => {
-            
-    }
-
+const ReusableInput = ({ type, placeholder, error, mode = 'standard', register }: Props) => {
     return (
-        <div className={styles.inputMain} onSubmit={handleSubmit(onSubmit)}>
-              <span className={styles.inputText}>{props.placeholder}</span>
-            <input
+        <div className={styles.inputMain}>
+              <span className={styles.inputText}>{placeholder}</span>
+              <input
                 className={`${styles.input} ${styles[mode]}`}
-                type={props.type === 'email' ? 'email' : 'password'}
-                placeholder={props.placeholder}
-                {...register(props.type, inputData)}
-                onFocus={(event) => {
-                    if (event.target.value === props.placeholder) {
-                        event.target.value = '';
-                    }
-                }}
+                type={type}
+                placeholder={placeholder}
+                {...register}
             />
         </div>
     );
