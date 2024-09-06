df9d4024c83a762f87483af55c692575ad11c598
giorgitsintsadzee <giorgi.tsintsadze26@gmail.com>
2024-09-06 18:02:53 +0400

    started register form

---
+++ app/Components/RegisterForm/RegisterForm.tsx
@@ -0,0 +1,36 @@
+'use client'
+import { useForm } from 'react-hook-form';
+import ReusableInput from '../ReusableInput/ReusableInput'
+import styles from './RegisterForm.module.scss'
+
+const RegisterForm = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isValid },
+    } = useForm();
+
+    const onSubmit = (data: any) => {
+        console.log(data);
+    };
+    return (
+        <div>
+            <ReusableInput
+                type='email'
+                placeholder='Enter email'
+                register={register('email', {
+                    required: 'Email is required',
+                    pattern: {
+                        value: /\S+@\S+\.\S+/,
+                        message: 'Entered value does not match email format',
+                    },
+                })}
+                mode={errors.email ? 'error' : isValid ? 'success' : 'standard'}
+
+            />
+
+        </div>
+    )
+}
+
+export default RegisterForm
