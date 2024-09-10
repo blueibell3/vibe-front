4c1b9558aa65471b19135bb3fcaca436fbab7115
giorgitsintsadzee <giorgi.tsintsadze26@gmail.com>
2024-09-10 16:25:15 +0400

    registration done

--- app/Components/RegisterForm/RegisterForm.tsx
+++ app/Components/RegisterForm/RegisterForm.tsx
@@ -10,15 +10,20 @@ const RegisterForm = () => {
         register,
         handleSubmit,
         formState: { errors, isValid },
+        watch,
     } = useForm();
 
     const onSubmit = (data: any) => {
         console.log(data);
     };
+
+    const password = watch('password');
+
     return (
         <form onSubmit={handleSubmit(onSubmit)} className={styles.conteiner}>
             <div className={styles.regist}>
                 <Image className={styles.logo} src='/logo.png' width={170} height={70} alt={"logo"} />
+                
                 <ReusableInput
                     type='email'
                     placeholder='Enter email'
@@ -31,6 +36,7 @@ const RegisterForm = () => {
                     })}
                     mode={errors.email ? 'error' : isValid ? 'success' : 'standard'}
                 />
+                
                 <ReusableInput
                     type="password"
                     placeholder="Enter Password"
@@ -43,28 +49,34 @@ const RegisterForm = () => {
                     })}
                     mode={errors.password ? 'error' : isValid ? 'success' : 'standard'}
                 />
+                
                 <ReusableInput
                     type="password"
-                    placeholder="Confirm password"
-                    register={register('password', {
-                        required: 'Password is required',
-                        minLength: {
-                            value: 8,
-                            message: 'Password must be at least 8 characters long',
-                        },
+                    placeholder="Confirm Password"
+                    register={register('confirmPassword', {
+                        required: 'Please confirm your password',
+                        validate: value =>
+                            value === password || 'this passwords did not match',
                     })}
-                    mode={errors.password ? 'error' : isValid ? 'success' : 'standard'}
+                    mode={errors.confirmPassword ? 'error' : isValid ? 'success' : 'standard'}
                 />
+                
+                {errors.confirmPassword && (
+                    <p className={styles.errorMessage}>
+                        {typeof errors.confirmPassword.message === 'string' && errors.confirmPassword.message}
+                    </p>
+                )}
+                
                 <div className={styles.buttonWrapper}>
                     <div className={styles.button}>
-                        <Button title={"Sign in"} type={"primary"} />
+                        <Button title={"Sign up"} type={"primary"} />
                     </div>
                 </div>
                 <div className={styles.clicklSignUp}>
-                    <a className={styles.signUpText} href="/authorisation">already have an account? Sign in</a>
+                    <a className={styles.signUpText} href="/authorisation">Already have an account? Sign in</a>
                 </div>
             </div>
         </form>
     )
 }
-export default RegisterForm
+export default RegisterForm;
