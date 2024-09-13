cf8c23d8b3528b39c27443b04a9e5cface58cace
giorgitsintsadzee <giorgi.tsintsadze26@gmail.com>
2024-09-13 17:38:28 +0400

    made log out  logic

--- app/Components/ProfileMobile/ProfileMobile.tsx
+++ app/Components/ProfileMobile/ProfileMobile.tsx
@@ -1,9 +1,20 @@
+'use client'
 import Image from 'next/image'
 import styles from './ProfileMobile.module.scss'
 import Button from '../Button/Button';
+import { useRouter } from 'next/navigation';
 
 
 const ProfileMobile = () => {
+    const router = useRouter();
+
+    const handleLogOut = () => {
+        // Delete the token from cookies
+        document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+
+        // Refresh the page to reflect the logout state
+        window.location.reload();
+    };
 
     return (
         <div className={styles.container}>
@@ -14,7 +25,7 @@ const ProfileMobile = () => {
                     <span className={styles.emailText}>G.sanikidze@gmail.com</span>
                 </div>
             </div>
-            <div className={styles.logOut}>
+            <div className={styles.logOut} onClick={handleLogOut}>
                 <Button title={'Log out'} type={'primary'} />
             </div>
         </div>
