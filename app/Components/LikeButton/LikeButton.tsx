a29595661599324e3a693890a97a4dafcda655bf
giorgitsintsadzee <giorgi.tsintsadze26@gmail.com>
2024-09-16 17:46:09 +0400

    everywhere adeed global id i guess

--- app/Components/LikeButton/LikeButton.tsx
+++ app/Components/LikeButton/LikeButton.tsx
@@ -3,9 +3,20 @@ import Image from 'next/image';
 import { useState } from 'react';
 import styles from './LikeButton.module.scss';
 import Link from 'next/link';
+import { useRecoilState } from 'recoil';
+import { currentTrackIndexState, globalMusicState } from '@/app/state';
 
-const LikeButton = () => {
+type Props = {
+    id: number
+    trackIndex: number
+}
+
+const LikeButton = (props: Props) => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [globalId] = useRecoilState(globalMusicState)
+    const [index] = useRecoilState(currentTrackIndexState)
+    console.log(globalId, "id")
+    console.log(index, 'inex')
 
     const handleClick = () => {
         setIsMenuOpen(!isMenuOpen);
@@ -16,11 +27,11 @@ const LikeButton = () => {
             <button onClick={handleClick} className={styles.button}>
                 <Image src="/icons/three dots.svg" alt="menu" width={24} height={24} />
             </button>
-            {isMenuOpen && (
+            {isMenuOpen && globalId === props.id && (
                 <div className={styles.menu} onClick={handleClick}>
                     <div className={styles.menuItem}>
                         <Link href='/playlist'>
-                            <div>+ Create playlist</div>
+                            <div> + Create playlist</div>
                         </Link>
                     </div>
                 </div>
