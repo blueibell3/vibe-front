3a06da451689b215b9ee44664d1f935d5d355bfa
giorgitsintsadzee <giorgi.tsintsadze26@gmail.com>
2024-09-18 15:26:16 +0400

    done successful

--- app/Components/ProfileMobile/ProfileMobile.tsx
+++ app/Components/ProfileMobile/ProfileMobile.tsx
@@ -3,8 +3,36 @@
 import Image from 'next/image'
 import styles from './ProfileMobile.module.scss'
 import Button from '../Button/Button';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 const ProfileMobile = () => {
+    const [email, setEmail] = useState<string>('');
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const token = getToken();
+
+                const response = await axios.get('https://vibe-backend-prrr.onrender.com/users/me', {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    }
+                });
+                setEmail(response.data.email);
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+            }
+        };
+
+        fetchUserData();
+    }, []);
+
+    const getToken = () => {
+        const match = document.cookie.match(new RegExp('(^| )token=([^;]+)'));
+        return match ? match[2] : '';
+    };
+
     const handleLogOut = () => {
         document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
         window.location.reload();
@@ -16,7 +44,9 @@ const ProfileMobile = () => {
                 <div className={styles.profileText}>Profile</div>
                 <div className={styles.userContainer}>
                     <Image src={'/profileuser.svg'} width={24} height={24} alt={'x'} />
-                    <span className={styles.emailText}>G.sanikidze@gmail.com</span>
+                    <span className={styles.emailText}>
+                        {email ? email : 'Loading...'}
+                    </span>
                 </div>
             </div>
             <div className={styles.logOut} onClick={handleLogOut}>
