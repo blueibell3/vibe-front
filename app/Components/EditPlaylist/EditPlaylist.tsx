7d904b5867b2692e53e3743dd72d8937774c70f2
lizi qerdikashvili <lizi.qerdikashvili@icloud.com>
2024-09-27 17:32:41 +0400

    create edit playlist

---
+++ app/Components/EditPlaylist/EditPlaylist.tsx
@@ -0,0 +1,105 @@
+'use client'
+import { useForm, SubmitHandler } from 'react-hook-form';
+import React, { useState } from 'react';
+import styles from './EditPlaylist.module.scss';
+import axios from 'axios';
+import EditPen from '../EditPen/EditPen';
+import Button from '../Button/Button';
+
+type EditListFormData = {
+    name: string;
+}
+
+type Props = {
+    playlistId: number
+}
+
+
+const EditPlaylist = (props: Props) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const { register, handleSubmit, reset, formState: { errors } } = useForm<EditListFormData>();
+
+    const handleOpenModal = () => setIsOpen(true);
+    const handleCloseModal = () => {
+        setIsOpen(false);
+        reset();
+    };
+
+    const handleDone = () => {
+        setIsOpen(false);
+        reset();
+    };
+    const handleLogOut = () => {
+    
+        window.location.reload(); 
+    };
+
+
+    const onSubmit: SubmitHandler<EditListFormData> = async (values: EditListFormData) => {
+        console.log(values);
+
+        const data = new FormData();
+        data.append('name', values.name);
+
+        try {
+            const token = document.cookie
+                .split('; ')
+                .find((row) => row.startsWith('token='))
+                ?.split('=')[1];
+
+            await axios.patch(`https://vibetunes-backend.onrender.com/playlist/${props.playlistId}`, data, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`
+                }
+            });
+
+           
+            handleDone();
+        } finally {
+          
+            setIsOpen(false);
+        }
+    };
+
+    return (
+        <>
+            <div onClick={handleOpenModal}>
+                <EditPen />
+            </div>
+            {
+                isOpen &&
+                <div className={styles.reausableModalContainer}>
+                    <div className={styles.reusableModal}>
+                    <div className={styles.addPlaylist}>
+                            <span className={styles.addPlaylistText}>EditPlaylist</span>
+                            <button onClick={handleCloseModal} className={styles.addPlaylistIcon}>
+                                <img src="/xicon.svg" alt="x" />
+                            </button>
+                        </div>
+                        <form onSubmit={handleSubmit(onSubmit)} className={styles.addname}>
+                            <span className={styles.musicText}>Name</span>
+                            <input
+                                className={styles.inputMusic}
+                                type="text"
+                                placeholder='Playlist name'
+                                {...register('name', { required: true })}
+                            />
+                            {errors.name && <span className={styles.error}>Name is required</span>}
+                            <div className={styles.modalButton}>
+                                <div className={styles.cancel} onClick={handleCloseModal}>
+                                    <Button title={'cancel'} type={'secondary'} />
+                                </div>
+                                <div className={styles.done} onClick={handleLogOut}>
+                                    <Button title={'done'} type={'primary'}/>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            }
+        </>
+    );
+}
+
+export default EditPlaylist;
--- app/Components/ListItem/ListItem.tsx
+++ app/Components/ListItem/ListItem.tsx
@@ -1,11 +1,12 @@
 import Link from 'next/link';
 import styles from './ListItem.module.scss';
+import EditPlaylist from '../EditPlaylist/EditPlaylist';
 
 
 type Props = {
   name: string;
   imgSrc?: string;
-  lastName?:string
+  lastName?: string
   isArtist: boolean;
   id: number;
   link: string;
@@ -15,17 +16,22 @@ const ListItem = (props: Props) => {
 
   return (
     <>
-      <Link href={props.link} className={styles.artistListItem}>
+      <div className={styles.artistListItem}>
         <div className={styles.artistListGap}>
-          <div className={styles.artistListContainer}>
-            <img className={styles.listImg} src={props.imgSrc} />
-            <span className={styles.listText}>{props.name}</span>
-            <span className={styles.listText}>{props.lastName}</span>
-
-          </div>
-          <img src="arrowicon.svg" alt="arrow" className={styles.arrow} />
+          <Link href={props.link}>
+            <div className={styles.artistListContainer}>
+              <img className={styles.listImg} src={props.imgSrc} />
+              <span className={styles.listText}>{props.name}</span>
+              <span className={styles.listText}>{props.lastName}</span>
+            </div>
+          </Link>
+           
         </div>
-      </Link>
+        <div className={styles.listItemEdit}>
+            <EditPlaylist playlistId={props.id} />
+            <img src="arrowicon.svg" alt="arrow" className={styles.arrow} />
+          </div>
+      </div>
     </>
   )
 }
