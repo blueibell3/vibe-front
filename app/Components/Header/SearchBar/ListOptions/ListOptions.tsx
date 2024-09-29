8396238d04541f913fd540cf426d2bfffc855abf
lizi qerdikashvili <lizi.qerdikashvili@icloud.com>
2024-09-29 19:39:29 +0400

    connect to backend

--- app/Components/Header/SearchBar/ListOptions/ListOptions.tsx
+++ app/Components/Header/SearchBar/ListOptions/ListOptions.tsx
@@ -4,9 +4,13 @@ import styles from './ListOptions.module.scss';
 type Props = {
     id: number;
     text: string;
-    img: string;
-    type: 'singer' | 'album';
-    link: string;
+    photo?: string;
+    file?: string;
+    type: 'album' | 'author' | 'music';
+    link?: string;
+    musicSrc?: string;
+    firstName?: string; 
+    lastName?: string;
 };
 
 interface ListOptionsProps {
@@ -15,27 +19,60 @@ interface ListOptionsProps {
 }
 
 const ListOptions = ({ options, onOptionClick }: ListOptionsProps) => {
+    const playMusic = (musicSrc: string) => {
+        const audio = new Audio(musicSrc);
+        audio.play();
+    };
+
     return (
         <ul className={styles.listOptions}>
             {options.map(option => (
-                <Link href={option.link} key={option.id}>
-                    <li className={styles.option}>
-                        <a
-                            onClick={() => {
-                                if (onOptionClick) {
-                                    onOptionClick(option.text);
-                                }
-                            }}
-                        >
+                <li className={styles.option} key={option.id}>
+                    {option.type === 'music' ? (
+                        <div onClick={() => playMusic(option.musicSrc!)}>
                             <img
-                                src={option.img}
-                                alt={option.text}
+                                src={option.photo}
+                                alt={'img'}
                                 className={`${styles.optionImage} ${styles[option.type]}`}
+                                width={200} 
+                                height={200} 
                             />
-                            <span>{option.text}</span>
-                        </a>
-                    </li>
-                </Link>
+                            <span className={styles.optionText}>{option.text}</span>
+                            {option.firstName && option.lastName && (
+                                <span className={styles.names}>{option.firstName} {option.lastName}</span>
+                            )}
+                        </div>
+                    ) : option.type === 'album' ? (
+                        <Link href={option.link!}>
+                            <div className={styles.artistAlbums}>
+                                <img
+                                    src={option.file}
+                                    alt={'img'}
+                                    className={`${styles.optionImage} ${styles[option.type]}`}
+                                />
+                                <div>
+                                    <div>
+                                        <span className={styles.optionText}>{option.text}</span>
+                                    </div>
+                                    {option.firstName && option.lastName && (
+                                        <span className={styles.names}>{option.firstName} {option.lastName}</span>
+                                    )}
+                                </div>
+                            </div>
+                        </Link>
+                    ) : (
+                        <Link href={option.link!}>
+                            <div className={styles.artistAlbums}>
+                                <img
+                                    src={option.file}
+                                    alt={'img'}
+                                    className={`${styles.optionImage} ${styles[option.type]}`}
+                                />
+                                <span className={styles.optionText}>{option.text}</span>
+                            </div>
+                        </Link>
+                    )}
+                </li>
             ))}
         </ul>
     );
--- app/Components/Header/SearchBar/SearchBar.tsx
+++ app/Components/Header/SearchBar/SearchBar.tsx
@@ -1,5 +1,6 @@
-    'use client';
+'use client';
 import { useState, useEffect, useCallback } from 'react';
+import axios from 'axios';
 import Image from 'next/image';
 import styles from './SearchBar.module.scss';
 import ListOptions from './ListOptions/ListOptions';
@@ -7,35 +8,122 @@ import ListOptions from './ListOptions/ListOptions';
 type Option = {
     id: number;
     text: string;
-    img: string;
-    type: 'singer' | 'album';
-    link: string;
+    img?: string;
+    type: 'album' | 'author' | 'music';
+    link?: string;
+    musicSrc?: string;
 };
 
-const listOptions: Option[] = [
-    { id: 1, text: 'Harry Styles', img: '/harryStyles.svg', type: 'singer', link: '/harry-styles' },
-    { id: 2, text: 'Havana', img: '/havana.svg', type: 'album', link: '/havana' },
-    { id: 3, text: 'Still Sane', img: '/', type: 'album', link: '/still-sane' },
-];
+interface Album {
+    firstName: string;
+    lastName: string;
+    id: number;
+    title: string;
+    file?: string;
+}
+
+interface Author {
+    id: number;
+    firstName: string;
+    lastName: string;
+    file?: string;
+}
+
+interface Music {
+    id: number;
+    name: string;
+    firstName: string;
+    lastName: string;
+    photo?: string;
+    audioSrc?: string;
+}
+
+interface ApiResponse {
+    album: Album[];
+    author: Author[];
+    music: Music[];
+}
 
 const SearchBar = () => {
     const [query, setQuery] = useState<string>('');
     const [filteredOptions, setFilteredOptions] = useState<Option[]>([]);
 
-    const filterOptions = useCallback(() => {
+    const fetchOptions = useCallback(async () => {
         if (query) {
-            const filtered = listOptions.filter(option =>
-                option.text.toLowerCase().includes(query.toLowerCase())
-            );
-            setFilteredOptions(filtered);
+            try {
+                const token = document.cookie
+                    .split('; ')
+                    .find((row) => row.startsWith('token='))
+                    ?.split('=')[1];
+
+                if (!token) {
+                    throw new Error('No token found');
+                }
+
+                const response = await axios.get<ApiResponse>(`https://vibetunes-backend.onrender.com/search/?searchField=${query}`, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+
+                const { album: albumData, author: authorData, music: musicData } = response.data;
+
+                if (!albumData || !authorData || !musicData) {
+                    console.error('Data missing in response:', response.data);
+                    setFilteredOptions([]);
+                    return;
+                }
+
+                const albumOptions = albumData.map((album: Album) => ({
+                    id: album.id,
+                    text:  album.title || 'Untitled',
+                    file: album.file || '/default-album.jpg',
+                    type: 'album' as const,
+                    link: `/album/${album.id}`,
+                    firstName: album.firstName, 
+                    lastName: album.lastName,
+                }));
+
+                const authorOptions = authorData.map((author: Author) => ({
+                    id: author.id,
+                    text: `${author.firstName} ${author.lastName}` || 'Unknown Author',
+                    file: author.file || '/default-author.jpg',
+                    type: 'author' as const,
+                    link: `/author/${author.id}`,
+                    firstName: author.firstName,
+                    lastName: author.lastName,
+                }));
+
+                const musicOptions = musicData.map((music: Music) => ({
+                    id: music.id,
+                    text: music.name || 'Untitled Music',
+                    photo: music.photo || '/default-music.jpg',
+                    type: 'music' as const,
+                    musicSrc: music.audioSrc || '',
+                    firstName: music.firstName,
+                    lastName: music.lastName,
+                }));
+
+                const allOptions = [...albumOptions, ...authorOptions, ...musicOptions];
+                setFilteredOptions(allOptions);
+
+            } catch (error) {
+                console.error('Error fetching search options:', error);
+                setFilteredOptions([]);
+            }
         } else {
             setFilteredOptions([]);
         }
     }, [query]);
 
     useEffect(() => {
-        filterOptions();
-    }, [query, filterOptions]);
+        fetchOptions();
+    }, [query, fetchOptions]);
+
+    const handleOptionClick = (text: string) => {
+        setQuery(text);
+    };
 
     return (
         <div className={styles.searchContainer}>
@@ -55,7 +143,7 @@ const SearchBar = () => {
             />
             <div className={styles.border}>
                 {filteredOptions.length > 0 && (
-                    <ListOptions options={filteredOptions} onOptionClick={setQuery} />
+                    <ListOptions options={filteredOptions} onOptionClick={handleOptionClick} />
                 )}
             </div>
         </div>
