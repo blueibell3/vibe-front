9397d03f1eb6315e55c4de9174b5532b452dd362
lizi qerdikashvili <lizi.qerdikashvili@icloud.com>
2024-10-02 13:54:05 +0400

    add top charts

--- app/(authorised)/topCharts/[id]/page.tsx
+++ app/(authorised)/topCharts/[id]/page.tsx
@@ -4,7 +4,7 @@ const Id = () => {
 
     return (
         <>
-            <TopChartsById />
+            <TopChartsById genreName={'rap'} />
         </>
     )
 }
--- app/Components/TopCharts/TopChartsById/TopChartsById.tsx
+++ app/Components/TopCharts/TopChartsById/TopChartsById.tsx
@@ -1,7 +1,62 @@
-import TrendHits from "../../TrendHits/TrendHits";
-import styles from "./TopChartsById.module.scss"
+'use client'
 
-const TopChartsById = () => {
+import { useParams } from "next/navigation";
+
+import styles from "./TopChartsById.module.scss";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import TrendHits from "../../TrendHits/TrendHits";
+
+type Music = {
+    id: number;
+    name: string;
+    artistName: string;
+};
+
+const genreMap: { [key: number]: string } = {
+    1: 'Techno',
+    2: 'House',
+    3: 'Rap',
+    4: 'Rock',
+};
+
+const TopChartsById: React.FC = () => {
+    const params = useParams();
+    const [musicData, setMusicData] = useState<Music[]>([]);
+    const [genreName, setGenreName] = useState<string>('');
+
+    useEffect(() => {
+        const genreId = Array.isArray(params.id) ? parseInt(params.id[0]) : parseInt(params.id);
+        if (genreMap[genreId]) {
+            setGenreName(genreMap[genreId]);
+        }
+
+        const fetchMusic = async () => {
+            try {
+                const token = document.cookie
+                    .split('; ')
+                    .find((row) => row.startsWith('token='))
+                    ?.split('=')[1];
+
+                if (!token) {
+                    throw new Error('No token found');
+                }
+
+                const response = await axios.get(`https://vibetunes-backend.onrender.com/genres/${genreId}/musics`, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+
+                setMusicData(response.data);
+            } catch (error) {
+                console.error('Error fetching music data:', error);
+            }
+        };
+
+        fetchMusic();
+    }, [params.id]);
 
     return (
         <>
