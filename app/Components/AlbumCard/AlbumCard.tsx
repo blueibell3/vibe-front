5fd71be91d30e1c0dd946c3ee97670bf3b3b8f42
giorgitsintsadzee <giorgi.tsintsadze26@gmail.com>
2024-10-02 18:34:34 +0400

    change some details

--- app/Components/AlbumCard/AlbumCard.tsx
+++ app/Components/AlbumCard/AlbumCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '../AlbumCard/AlbumCard.module.scss'
+import styles from '../AlbumCard/AlbumCard.module.scss';
 import Link from 'next/link';
 
 type Props = {
@@ -8,12 +8,12 @@ type Props = {
   imageUrl: any;
   artistName?: string;
   releaseDate: string;
-}
+};
 
 const AlbumCard = (props: Props) => {
   return (
     <Link className={styles.albumCard} href={`/albums/${props.id}`}>
-      <img className={styles.image} src={props.imageUrl} alt="image" />
+      <img className={styles.image} src={props.imageUrl} alt={props.songName} />
       <div className={styles.albumCardText}>
         <div className={styles.title}>
           <span className={styles.songName}>{props.songName}</span>
@@ -22,7 +22,7 @@ const AlbumCard = (props: Props) => {
         <span className={styles.artistName}>{props.artistName}</span>
       </div>
     </Link>
-  )
-}
+  );
+};
 
-export default AlbumCard
+export default AlbumCard;
